8f468b27ed667e1b202ac0feb5b3822bfd99f7d4
MPFilatov <97291329+MPFilatov@users.noreply.github.com>
2023-05-18 22:28:43 +0300

    remove config.php

---
+++ api/config.php
@@ -0,0 +1,3 @@
+<?php
+const DB_HOST_LOCAL = "127.0.0.1";
+const DB_HOST_REMOTE = "127.0.0.1";
---
+++ api/config_example.php
@@ -0,0 +1,11 @@
+<?php
+const DB_HOST_LOCAL = "127.0.0.1";
+const DB_HOST_REMOTE = "127.0.0.1";
+const DB_USER_LOCAL = "root";
+const DB_USER_REMOTE = "cu71509_chess01";
+const DB_PASSWORD_LOCAL = "";
+const DB_PASSWORD_REMOTE = "ywg13XLn";
+const DB_NAME_LOCAL = "chess";
+const DB_NAME_REMOTE = "cu71509_chess01";
+const DB_CHARSET = "utf8";
+const DB_TIMEZONE = "+03:00";
